2c4fec3238306ff42e90bf523678055f206e7b6a
yuandalong <yuandalong@139.com>
2017-03-13 10:19:09 +0800

    add yield demo

---
+++ src/main/scala/com/ydl/learning/scala/ArrayDemo.scala
@@ -0,0 +1,30 @@
+package com.ydl.learning.scala
+
+
+object ArrayDemo {
+  def main(args: Array[String]): Unit = {
+    val array = new Array[String](3)
+    array(0) = "hello"
+    array(1) = ", "
+    array(2) = "world"
+    array.foreach {
+      print
+    }
+    println()
+    //for循环
+    for (i <- array) print(i)
+    println()
+
+    //for循环加过滤
+    for (i <- array if i == "world") print(i)
+    println()
+
+    //for循环返回过滤结果，yield关键字
+    def forYield(array: Array[Int]) = {
+      for (i <- array if (i > 3)) yield i
+    }
+
+    val intArray = Array(1,2,3,4,5)
+    forYield(intArray).foreach(print)//过滤后的结果
+  }
+}
