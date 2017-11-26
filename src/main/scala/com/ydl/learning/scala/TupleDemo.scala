b953733a9fa3fa73bfb83db6f3164dbcba9994e2
袁大龙 <yuandalong@yazuo.com>
2017-11-26 21:59:53 +0800

    update

--- src/main/scala/com/ydl/learning/scala/TupleDemo.scala
+++ src/main/scala/com/ydl/learning/scala/TupleDemo.scala
@@ -6,29 +6,43 @@ package com.ydl.learning.scala
   * Created by ydl on 2017/3/20.
   */
 object TupleDemo extends App {
-  //元组的定义
-  ("hello", "china", "beijing")
-  //res23: (String, String, String) = (hello,china,beijing)
+    //元组的定义
+    ("hello", "china", "beijing")
+    //res23: (String, String, String) = (hello,china,beijing)
 
-  ("hello", "china", 1)
-  //res24: (String, String, Int) = (hello,china,1)
+    ("hello", "china", 1)
+    //res24: (String, String, Int) = (hello,china,1)
 
-  var tuple = ("Hello", "China", 1)
-  //tuple: (String, String, Int) = (Hello,China,1)
+    var tuple = ("Hello", "China", 1)
+    //tuple: (String, String, Int) = (Hello,China,1)
 
-  //访问元组内容
-  tuple._1
-  //res25: String = Hello
+    //访问元组内容，_表示元组下班，从1开始
+    tuple._1
+    //res25: String = Hello
 
-  tuple._2
-  //res26: String = China
+    tuple._2
+    //res26: String = China
 
-  tuple._3
-  //res27: Int = 1
+    tuple._3
+    //res27: Int = 1
 
-  //通过模式匹配获取元组内容
-  val (first, second, third) = tuple
-  println(first)
-  println(second)
-  println(third)
+    //通过模式匹配获取元组内容
+    val (first, second, third) = tuple
+    println(first)
+    println(second)
+    println(third)
+    //元组可以很好得与模式匹配相结合。
+    val t = ("java", 1)
+    t match {
+        case ("java", count) => {
+            println(count + " is java")
+        }
+        case (lang, count) => {
+            println(lang + "" + count)
+        }
+    }
+
+    //在创建两个元素的元组时，可以使用特殊语法：->
+    val a = 1 -> 2
+    println(a._1)
 }
