ccab702e1a462cffb934cf2f929c1cd6090c3e1d
袁大龙 <yuandalong@yazuo.com>
2018-01-18 22:56:23 +0800

    update apply demo

--- src/main/scala/com/ydl/learning/scala/ApplyDemo.scala
+++ src/main/scala/com/ydl/learning/scala/ApplyDemo.scala
@@ -17,11 +17,14 @@ object ApplyDemo extends App {
     //调用伴生对象的apply方法，可以不用new来创建实例，伴生对象通常作为工厂使用
     val bar = Bar("123")
 }
-//单例对象可以和类具有相同的名称，此时该对象也被称为“伴生对象”。我们通常将伴生对象作为工厂使用。
 
+//单例对象可以和类具有相同的名称，此时该对象也被称为“伴生对象”。我们通常将伴生对象作为工厂使用。
 //下面是一个简单的例子，可以不需要使用’new’来创建一个实例了。
 class Bar(foo: String)
 
 object Bar {
-    def apply(foo: String) = new Bar(foo)
+    def apply(foo: String) = {
+        println(foo)
+        new Bar(foo)
+    }
 }
