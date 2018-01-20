0ae6e08c9e03db5217a2989633a0e84931135f3c
yuandalong <yuandalong@139.com>
2018-01-20 22:56:15 +0800

    add apply method demo

---
+++ src/main/scala/com/ydl/learning/scala/ApplyOperation.scala
@@ -0,0 +1,27 @@
+package com.ydl.learning.scala
+
+/**
+  * Created by admin on 2018/1/20.
+  */
+class ApplyOperation {
+}
+class ApplyTest{
+    def apply() = println("I am into spark so much!!!")
+    def haveATry: Unit ={
+        println("have a try on apply")
+    }
+}
+object ApplyTest{
+    def apply() = {
+        println("I  am into Scala so much")
+        new ApplyTest
+    }
+}
+object ApplyOperation{
+    def main (args: Array[String]) {
+        val a = ApplyTest() //这里就是使用object 的使用
+
+        a.haveATry
+        a() // 这里就是 class 中 apply使用
+    }
+}
