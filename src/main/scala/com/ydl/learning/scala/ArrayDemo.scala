e23a2b8c351dd32316af810e902b831f1f79ad66
ydl <yuandalong@139.com>
2018-06-12 17:42:13 +0800

    数组倒序循环

--- src/main/scala/com/ydl/learning/scala/ArrayDemo.scala
+++ src/main/scala/com/ydl/learning/scala/ArrayDemo.scala
@@ -14,21 +14,23 @@ object ArrayDemo {
     //for循环
     for (i <- array) print(i)
     println()
-
+    //倒序
+    for(i <- array.reverse) print(i)
+    println()
     //for循环加过滤
     for (i <- array if i == "world") print(i)
     println()
 
     //for循环返回过滤结果，yield关键字
     def forYield(array: Array[Int]) = {
-      for (i <- array if (i > 3)) yield i
+      for (i <- array if i > 3) yield i
     }
 
     val intArray = Array(1, 2, 3, 4, 5)
     forYield(intArray).foreach(print) //过滤后的结果
 
     //定义对象时直接初始化
-    val a = Array[String]("a","b")
+    val a = Array[String]("a", "b")
     a.foreach(println)
   }
 }
